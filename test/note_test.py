88ab126c1006aec27e075dbf4f3c5935f3ce55df
youqingkui <youqingkui@qq.com>
2016-01-27 22:49:12 +0800

    test change image ok

--- test/note_test.py
+++ test/note_test.py
@@ -4,6 +4,8 @@
 import requests
 import os
 import hashlib
+import binascii
+
 from lib.Evernote import EvernoteMethod
 import evernote.edam.type.ttypes as Types
 from bs4 import BeautifulSoup
@@ -20,27 +22,48 @@ headers = {'User-Agent':'osee2unifiedRelease/332 CFNetwork/711.3.18 Darwin/14.0.
 
 
 
-url = 'https://api.zhihu.com/answers/83350780'
+url = 'https://api.zhihu.com/answers/83464968'
 
 r = requests.get(url, headers=headers)
 res_json = r.json()
 content = res_json.get('content', '')
 soup = BeautifulSoup(content, "html5lib")
 # soup = BeautifulSoup(content)
-
 def remove_attrs(tag):
     attrs = tag.attrs
     for k, v in attrs.items():
         if k not in ['src', 'href']:
             del tag.attrs[k]
 
+def change_img(tags):
+    img_arr = [img.attrs['src'] for img in tags if img['src']]
+    resources = EvernoteMethod.getRemoteRes(img_arr)
+    for index, res in enumerate(resources):
+        hexhash = binascii.hexlify(res.data.bodyHash)
+        new_tag = soup.new_tag('en-media')
+        new_tag['type'] = res.mime
+        new_tag['hash'] = hexhash
+        # print(new_tag)
+        # print(tags[index])
+        tags[index].replace_with(new_tag)
+
+    return resources
+
+
+
+
+
+
+
 soup.find_all(remove_attrs)
+res = change_img(soup.find_all("img"))
 
 soup.html.unwrap()
 soup.head.unwrap()
 soup.body.unwrap()
 
+# print soup.prettify()
 print(str(soup))
-
+#
 html_content = str(soup)
-EvernoteMethod.makeNote(noteStore, 'hi', html_content)
+EvernoteMethod.makeNoteWithRes(noteStore, 'hi', html_content, res)
