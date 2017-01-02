9f1ffeab9cb608af3fbac337048f559911a761cd
youqingkui <youqingkui@qq.com>
2017-01-02 12:28:45 +0800

    add logger print

--- lib/logger_fun.py
+++ lib/logger_fun.py
@@ -6,7 +6,7 @@ import time
 
 def get_logger():
     logger = logging.getLogger("threading_example")
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     date_info = time.strftime('%Y-%m-%d')
     fh = logging.FileHandler("/data/www/logs/zhuanlan.log.%s" % date_info)
     # fh = logging.FileHandler("/Users/youqingkui/PycharmProjects/zhihufav/zhuanlan.log.%s" % date_info)
@@ -14,8 +14,17 @@ def get_logger():
     formatter = logging.Formatter(fmt)
     fh.setFormatter(formatter)
 
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
     logger.addHandler(fh)
+    logger.addHandler(ch)
     return logger
 
 
 logger = get_logger()
+
+if __name__ == '__main__':
+    logger.debug("hello debug")
+    logger.warning("hello warning")
+    logger.info("hello info")
+    logger.error("hello error")
